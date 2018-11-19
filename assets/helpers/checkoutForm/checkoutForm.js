fb48b99c5542964853166d46da88c6a8b20521dc
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2018-11-19 17:10:37 +0000

    add comment

--- assets/helpers/checkoutForm/checkoutForm.js
+++ assets/helpers/checkoutForm/checkoutForm.js
@@ -87,13 +87,23 @@ export const formElementIsValid = (formElement: Object | null) => {
 
 export const formIsValid = (formClassName: string) => formElementIsValid(getForm(formClassName));
 
-export function checkoutFormShouldSubmit(
+type CheckoutFormShouldSubmitParameters = {|
   contributionType: Contrib,
   isSignedIn: boolean,
   isRecurringContributor: boolean,
   userTypeFromIdentityResponse: UserTypeFromIdentityResponse,
   formValid: boolean,
-) {
+|}
+
+export function checkoutFormShouldSubmit(params: CheckoutFormShouldSubmitParameters) {
+  const {
+    formValid,
+    contributionType,
+    isSignedIn,
+    userTypeFromIdentityResponse,
+    isRecurringContributor,
+  } = params;
+
   return formValid
     && canContributeWithoutSigningIn(contributionType, isSignedIn, userTypeFromIdentityResponse)
     && !isRecurringContributor;
