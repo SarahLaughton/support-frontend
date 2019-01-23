dc4b14415a81a0409db592c8be0e4375faa17af2
tomrf1 <tom.forbes@theguardian.com>
2019-01-23 12:58:13 +0000

    remove annual amounts test

--- assets/helpers/abTests/abtestDefinitions.js
+++ assets/helpers/abTests/abtestDefinitions.js
@@ -3,20 +3,5 @@ import type { Tests } from './abtest';
 
 // ----- Tests ----- //
 
-export type AnnualContributionsTestVariant = 'control' | 'annualAmountsA' | 'notintest';
-
 export const tests: Tests = {
-  annualContributionsRoundThree: {
-    variants: ['control', 'annualAmountsA'],
-    audiences: {
-      ALL: {
-        offset: 0,
-        size: 1,
-      },
-    },
-    isActive: false,
-    independent: true,
-    seed: 3,
-  },
-
 };
